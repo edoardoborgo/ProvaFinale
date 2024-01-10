3f2e1edb30b02c793a39ff14b134b47837adcb97
albepenz <151561592+albepenz@users.noreply.github.com>
2024-01-10 18:45:12 +0100

    update

---
+++ cmake-build-debug/CMakeFiles/ProvaFinale.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/albertopenzo/CLionProjects/ProvaFinale/src/Board.cpp" "CMakeFiles/ProvaFinale.dir/src/Board.cpp.o" "gcc" "CMakeFiles/ProvaFinale.dir/src/Board.cpp.o.d"
+  "/Users/albertopenzo/CLionProjects/ProvaFinale/src/BoardBoxBlank.cpp" "CMakeFiles/ProvaFinale.dir/src/BoardBoxBlank.cpp.o" "gcc" "CMakeFiles/ProvaFinale.dir/src/BoardBoxBlank.cpp.o.d"
+  "/Users/albertopenzo/CLionProjects/ProvaFinale/src/BoardBoxProperty.cpp" "CMakeFiles/ProvaFinale.dir/src/BoardBoxProperty.cpp.o" "gcc" "CMakeFiles/ProvaFinale.dir/src/BoardBoxProperty.cpp.o.d"
+  "/Users/albertopenzo/CLionProjects/ProvaFinale/src/BoardManager.cpp" "CMakeFiles/ProvaFinale.dir/src/BoardManager.cpp.o" "gcc" "CMakeFiles/ProvaFinale.dir/src/BoardManager.cpp.o.d"
+  "/Users/albertopenzo/CLionProjects/ProvaFinale/src/Human.cpp" "CMakeFiles/ProvaFinale.dir/src/Human.cpp.o" "gcc" "CMakeFiles/ProvaFinale.dir/src/Human.cpp.o.d"
+  "/Users/albertopenzo/CLionProjects/ProvaFinale/src/Pc.cpp" "CMakeFiles/ProvaFinale.dir/src/Pc.cpp.o" "gcc" "CMakeFiles/ProvaFinale.dir/src/Pc.cpp.o.d"
+  "/Users/albertopenzo/CLionProjects/ProvaFinale/src/Player.cpp" "CMakeFiles/ProvaFinale.dir/src/Player.cpp.o" "gcc" "CMakeFiles/ProvaFinale.dir/src/Player.cpp.o.d"
+  "/Users/albertopenzo/CLionProjects/ProvaFinale/src/main.cpp" "CMakeFiles/ProvaFinale.dir/src/main.cpp.o" "gcc" "CMakeFiles/ProvaFinale.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
